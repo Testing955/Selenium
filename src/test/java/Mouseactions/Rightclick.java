024e0189a41445d361a7a6a1edd9db02cb130d17
Prince <prince.princeojha@gmail.com>
2024-07-25 01:45:10 +0530

    secondchange

--- src/test/java/Mouseactions/Rightclick.java
+++ src/test/java/Mouseactions/Rightclick.java
@@ -7,26 +7,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+public class rightClick {
 
-public class Rightclick 
-{
-
-	public static void main(String[] args) throws InterruptedException 
-	{
-WebDriverManager.chromedriver().setup();
-WebDriver driver= new ChromeDriver();
-driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
-driver.manage().window().maximize();
-driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-WebElement button = driver.findElement(By.xpath("/html/body/div/section/div/div/div/p/span"));
-Actions act = new Actions(driver);	
-act.contextClick(button).perform();
-driver.findElement(By.xpath("/html/body/ul/li[3]/span")).click();
-//Thread.sleep(5000);
-driver.switchTo().alert().accept();
-
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+		driver.findElement(By.xpath("//span[text()='Copy']"));
+		WebElement rightClick=driver.findElement(By.xpath("//span[text()='right click me']"));
+		Actions act = new Actions(driver);
+		act.contextClick(rightClick).perform();
+		WebElement copy= driver.findElement(By.xpath("//span[text()='Copy']"));
+		copy.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.switchTo().alert().accept();
+		driver.quit();
 
 	}
 
